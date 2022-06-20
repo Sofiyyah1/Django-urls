bc6b6f924f496ceec03416eddebe05cfbd8cbdac
Sofiyyah1 <sofiyattaiwo139@gmail.com>
2022-06-20 10:00:45 +0100

    Hide Secret Key

--- taiwo_Abdulsalam/taiwo_Abdulsalam/settings.py
+++ taiwo_Abdulsalam/taiwo_Abdulsalam/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t0(^7omszc8fam2f%z^u+xfx@(!fqc253ci0aupdx^a$wt7y2!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
